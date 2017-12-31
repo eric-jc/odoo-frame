aa3961e0928618b80d42d79e0f8497adb9f8a2fe
Eric <xxbregister@126.com>
2017-12-31 11:04:32 +0800

    单据类型，添加文字变量

--- utils/bill_define.py
+++ utils/bill_define.py
@@ -1,25 +1,46 @@
 # -*- coding: utf-8 -*-
 
+# 销售
+S_FORECAST_1 = 1
+S_ORDER_2 = 2
+S_RETURN_3 = 3
+
+# 仓储
+ST_OUT_STORE_20 = 20
+ST_OTHER_IN_21 = 21
+ST_OTHER_OUT_22 = 22
+ST_SALE_RETURN_23 = 23
+ST_STOCK_CHECK_24 = 24
+ST_TRANSFER_IN_25 = 25
+ST_TRANSFER_OUT_26 = 26
+
+# 财务
+F_SALE_ACCOUNT_40 = 40
+
+# 活禽采购
+PP_REQUEST_60 = 60
+PP_DISPATCH_CAR_61 = 61
+
 BILL_TYPE = [
     # 销售
-    (1, '销售预报'),
-    (2, '销售订单'),
-    (3, '销售退单'),
+    (S_FORECAST_1, '销售预报'),
+    (S_ORDER_2, '销售订单'),
+    (S_RETURN_3, '销售退单'),
 
     # 仓储
-    (20, '销售出库'),
-    (21, '其他入库'),
-    (22, '其他出库'),
-    (23, '销售退库'),
-    (24, '库存盘点'),
-    (25, '调拨入库'),
-    (26, '调拨出库'),
+    (ST_OUT_STORE_20, '销售出库'),
+    (ST_OTHER_IN_21, '其他入库'),
+    (ST_OTHER_OUT_22, '其他出库'),
+    (ST_SALE_RETURN_23, '销售退库'),
+    (ST_STOCK_CHECK_24, '库存盘点'),
+    (ST_TRANSFER_IN_25, '调拨入库'),
+    (ST_TRANSFER_OUT_26, '调拨出库'),
 
     # 财务
-    (40, '销售账单'),
+    (F_SALE_ACCOUNT_40, '销售账单'),
 
     # 活禽采购
-    (60, '订购申请'),
-    (61, '采购派车'),
+    (PP_REQUEST_60, '订购申请'),
+    (PP_DISPATCH_CAR_61, '采购派车'),
 
 ]
